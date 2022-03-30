30aa20a9c53ae93208b64dcbf282076f3ec1207d
subham10032002 <88156667+subham10032002@users.noreply.github.com>
2022-03-30 11:58:04 +0530

    Implemented chat activity UI

--- app/src/main/java/com/example/chitchat/ChatActivity.kt
+++ app/src/main/java/com/example/chitchat/ChatActivity.kt
@@ -2,12 +2,24 @@ package com.example.chitchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.vanniktech.emoji.EmojiManager
+import com.vanniktech.emoji.google.GoogleEmojiProvider
+import kotlinx.android.synthetic.main.activity_chat.*
+
 const val UID = "uid"
 const val NAME = "name"
 const val IMAGE = "photo"
 class ChatActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        EmojiManager.install(GoogleEmojiProvider())
         setContentView(R.layout.activity_chat)
+
+
+
     }
 }
